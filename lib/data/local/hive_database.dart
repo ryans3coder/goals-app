a50ef309946b67226248ef1fa37385604a9ce3ab
ryans3coder <ryansanb3@gmail.com>
2026-01-07 13:40:30 -0400

    Fix Category import ambiguity

--- lib/data/local/hive_database.dart
+++ lib/data/local/hive_database.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' hide Category;
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
